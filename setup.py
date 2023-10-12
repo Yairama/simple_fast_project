6adf882e83736a3e8e01049303a688af5c73da6c
Yairama <malmco.camborda@minsur.com>
2023-10-12 15:55:47 -0500

    added pip support

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sfp",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'sfp = simple_fast_project.main:run',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        '': ['*.exe'],
+    },
+    author="Yair Camborda",
+    author_email="yairoriginal@gmail.com",
+    description="Una herramienta para ejecutar sfp.exe",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/Yairama/simple_fast_project",
+)
